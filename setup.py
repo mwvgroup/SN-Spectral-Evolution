fdc3de23bea456eaa764c5cdef5788afa8d56d83
djperrefort <djperrefort@pitt.edu>
2020-01-08 16:03:59 -0500

    Adds setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+
+from setuptools import find_packages, setup
+
+setup(name='spec_analysis',
+      packages=find_packages(),
+      python_requires='>=3.8',
+      setup_requires=['pytest-runner'],
+      tests_require=['pytest'],
+      package_data={'': ['*.npy', '*.yml']},
+      include_pacakge_data=True)
